016548fadf56e49120d386fdfe964444ead7f66f
imadbenta <imad.benta@gmail.com>
2025-04-24 22:07:10 +0100

    Update frontend API service to use environment variables

--- frontend/src/services/bookService.js
+++ frontend/src/services/bookService.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/books";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const BOOKS_URL = `${API_URL}/books`;
 
-export const getBooks = () => axios.get(API_URL);
-export const getBook = (id) => axios.get(`${API_URL}/${id}`);
-export const createBook = (data) => axios.post(API_URL, data);
-export const updateBook = (id, data) => axios.put(`${API_URL}/${id}`, data);
-export const deleteBook = (id) => axios.delete(`${API_URL}/${id}`);
+export const getBooks = () => axios.get(BOOKS_URL);
+export const getBook = (id) => axios.get(`${BOOKS_URL}/${id}`);
+export const createBook = (data) => axios.post(BOOKS_URL, data);
+export const updateBook = (id, data) => axios.put(`${BOOKS_URL}/${id}`, data);
+export const deleteBook = (id) => axios.delete(`${BOOKS_URL}/${id}`);
